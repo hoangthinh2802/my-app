058c85561bc09b8c7e18ee7bb2e525b6648408db
hoangthinh2802 <hoangvanthinh9713@gmail.com>
2022-09-18 10:30:48 +0900

    pokemon game

---
+++ pokemon/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{ Component } from 'react';
+import Pokedex from './Pokedex';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Pokedex />
+      </div>
+
+    )
+  }
+}
+
+export default App;
